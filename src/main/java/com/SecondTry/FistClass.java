fa8d83a29c5f18fb5cced09a5d3ed797ffc89cf4
nangudeprashant <30728347+nangudeprashant@users.noreply.github.com>
2018-05-29 20:02:08 +0530

    New line added.

---
+++ src/main/java/com/SecondTry/FistClass.java
@@ -0,0 +1,8 @@
+package com.SecondTry;
+
+public class FistClass {
+public static void main(String[] args) {
+	System.out.println("Hi team.");
+	System.out.println("This is a new line added.");
+}
+}
